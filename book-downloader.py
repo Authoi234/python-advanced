6d217f21e12ea2b3ae35a93d26cb6834bf2cd0d5
Authoi234 <jawadkhan.authoi@gmail.com>
2025-07-20 14:19:48 +0600

    book downloader updated

--- book-downloader.py
+++ book-downloader.py
@@ -2,13 +2,14 @@ import requests
 import sys
 
 base_url = input("Please enter url: ", ) or "https://bugs.python.org/file47781/Tutorial_EDIT.pdf"
+name = input("Please enter name of the book (just file name, not .pdf or anything): ", ) or "downloaded_book"
 
 response = requests.get(base_url)
 
 if response.ok is False:
     sys.exit("Error downloading the file")
 
-with open("cpbook.pdf", "wb") as fp:
+with open(f"{name}.pdf", "wb") as fp:
     fp.write(response.content)
 
-print("Book downloaded successfully as cpbook.pdf")
+print("Book downloaded successfully as downloaded_book.pdf")
