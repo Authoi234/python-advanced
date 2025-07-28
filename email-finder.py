cb6fb96770506e2a8f1ddeded9e99311b0cd9638
Authoi234 <jawadkhan.authoi@gmail.com>
2025-07-28 21:55:34 +0600

    bug done

--- email-finder.py
+++ email-finder.py
@@ -2,6 +2,6 @@ import re
 
 text="To ask about books, please email at, book at sovietbooks [dot] com . Please email here for any support, support (at) sovietbooks dot com . For any problem plase email at, help.support@sovietbooks.com"
 
-text = re.sub(r'\s+[\[(]*\s*at\s*[\)\]]*\s+', "@", text, flags=re.I)
+text = re.sub(r'\s+[\(\[]*\s*at\s*[\)\]]*\s+', "@", text, flags=re.I)
 
 print(text)
